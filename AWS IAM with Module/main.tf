926cbe88fa5ac65a49ad55a819a544c493fddf2c
Mohammed Amjad <62937446+Amjad-DevSecOps@users.noreply.github.com>
2024-07-16 15:21:23 +0530

    Update main.tf

--- AWS IAM with Module/main.tf
+++ AWS IAM with Module/main.tf
@@ -4,3 +4,13 @@ module "iam_iam-user" {
   # insert the 1 required variable here
   name = "max"
 }
+
+#to remove default creation of iam_user_login and iam_access_key
+module "iam_iam-user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "5.28.0"
+  # insert the 1 required variable here
+  name                          = "sam"
+  create_iam_user_login_profile = false
+  create_iam_access_key         = false
+}
